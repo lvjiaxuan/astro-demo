ab55dfb6c2e39fdbb010df1efa273aa06a693706
lvjiaxuan <11309921+lvjiaxuan@users.noreply.github.com>
2024-07-12 15:56:30 +0800

    chore: still working

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig({
   site: 'https://lvjiaxuan.github.io',
   base: 'astro-demo',
   integrations: [vue(), UnoCSS()],
+  vite: {
+    css: {
+      transformer: 'lightningcss',
+    },
+  },
 })
